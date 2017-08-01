ee55c3edc83071eb645f36bcaacbcf14e6af4923
robrownn <robrownn@gmail.com>
2017-08-01 17:38:38 -0400

    Added set command with basic functionality.

---
+++ lib/run_tracker/commands/set.rb
@@ -0,0 +1,13 @@
+module RunTracker
+  module CommandLoader
+
+    module Set
+      extend Discordrb::Commands::CommandContainer
+      command(:set, description: "Allows the setting and changing of access level requirements for certain commands server-wide.",
+                    usage: "set <command> <value>", min_args: 2,
+                    required_permissions: [:manage_server]) do |event, command, value|
+        next "Test"
+
+      end
+    end
+  end
