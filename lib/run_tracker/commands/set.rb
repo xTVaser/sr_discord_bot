41bae0ef82500a1b96cd7e63f58930d4f9111a4a
robrownn <robrownn@gmail.com>
2017-08-02 11:46:12 -0400

    Added rate limiting to set command.

--- lib/run_tracker/commands/set.rb
+++ lib/run_tracker/commands/set.rb
@@ -2,12 +2,14 @@ module RunTracker
   module CommandLoader
     module Set
       extend Discordrb::Commands::CommandContainer
-      command(:set, description: 'Allows the setting and changing of access level requirements for certain commands server-wide.',
+      bucket :limiter, limit: 3, time_span: 60, delay: 10
+      command(:set, bucket: :limiter,
+                    description: 'Allows the setting and changing of access level requirements for certain commands server-wide.',
                     usage: '!set <command> <value>',
+                    rate_limit_message: 'Andy Gavin says to wait %time% more second(s)!',
                     min_args: 2) do |_event, _command, _value|
 
         RTBot.send_message(DevChannelID, "Test")
-
 
       end
     end
