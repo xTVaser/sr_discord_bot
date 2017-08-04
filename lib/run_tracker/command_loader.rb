c72c9b0f69144a96012d5bc7bdde16e386a9b7c1
robrownn <robrownn@gmail.com>
2017-08-04 17:32:35 -0400

    Set command functionality WIP

--- lib/run_tracker/command_loader.rb
+++ lib/run_tracker/command_loader.rb
@@ -5,12 +5,33 @@ module RunTracker
       require file
     end
 
+    # Constants
+    CONST_MANAGER = 2
+    CONST_MOD = 1
+    CONST_USER = 0
+
     # Add Command module names here
     @commands = [
       AddGame,
       List,
       Set
     ]
+
+    def self.getCommands
+      @commands
+    end
+
+    def self.checkCommands(_command)
+      @commands.each do |command|
+        if _command == command
+          return true
+        end
+      end
+
+      return false
+    end
+
+
 
     def self.loadCommands
       @commands.each do |command|
--- lib/run_tracker/commands/set.rb
+++ lib/run_tracker/commands/set.rb
@@ -1,15 +1,43 @@
 module RunTracker
   module CommandLoader
     module Set
+      # Require all command files.
+      Dir["#{File.dirname(__FILE__)}/commands/*.rb"].each do |file|
+        require file
+      end
       extend Discordrb::Commands::CommandContainer
-      bucket :limiter, limit: 3, time_span: 60, delay: 10
+
+      # Bucket for rate limiting. Limits to x uses every y seconds at z intervals.
+      bucket :limiter, limit: 3, time_span: 10, delay: 1
+
+
       command(:set, bucket: :limiter,
                     description: 'Allows the setting and changing of access level requirements for certain commands server-wide.',
-                    usage: '!set <command> <value>',
+                    usage: '!set <command> <value> ()',
                     rate_limit_message: 'Andy Gavin says to wait %time% more second(s)!',
-                    min_args: 2) do |_event, _command, _value|
+                    min_args: 2) do |event, command, value|
 
-        RTBot.send_message(DevChannelID, "Test")
+        # Command Body
+        #begin
+          #if (value > 2 || value < 0)
+          #  RTBot.send_message(event.channel, "Invalid value! Values can range from 0 to 2 (0 = User, 1 = Mod, 2 = Admin)")
+          if command.casecmp('addgame')
+            command = AddGame
+            command.permission_level(value)
+            RTBot.send_message(event.channel, "Permissions set for `addgame`!")
+          end
+        #rescue
+          #RTBot.send_message(_event.channel, "Command Not Found!")
+        #end
+
+
+        #@commands.each do |command|
+        #  if type.casecmp('command').zero?
+        #    RTBot.send_message(_event.channel, "Invalid Command! Type `!list commands` to see the list of commands this bot has.")
+        #  end
+        #end
+
+        #RTBot.send_message(DevChannelID, "Test")
 
       end
     end
