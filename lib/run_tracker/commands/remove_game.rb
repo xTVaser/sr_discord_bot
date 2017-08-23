7b376a1c6517a41ccd342377f469bb40f8ecd6cf
Tyler Wilding <xtvaser@gmail.com>
2017-08-22 21:28:12 -0400

    Can now remove games

--- lib/run_tracker/commands/remove_game.rb
+++ lib/run_tracker/commands/remove_game.rb
@@ -7,31 +7,56 @@ module RunTracker
                            usage: '!removegame <game-alias>',
                            permission_level: PERM_ADMIN,
                            min_args: 1,
-                           max_args: 1) do |event, search_field|
+                           max_args: 1) do |_event, _gameAlias|
 
-        "yes"
-        # if search_field.empty?
-        #   #event << '`game-alias` field is empty. Please enter a game alias to remove said game.'
-        # else
-        #   #removeGameById(event, search_field)
-        # end
+        # Check to see if the game is even tracked
+        gameID = PostgresDB.findID(_gameAlias)
+        if gameID == nil
+          _event << "That game is not currently being tracked"
+          next
+        end
+
+        begin
+
+          PostgresDB::Conn.transaction do |conn|
+
+            # Delete the tracked game row
+            conn.exec("DELETE from public.tracked_games where game_id = '#{gameID}'")
+
+            # Go through all of the runners and delete the tracked game
+            runners = PostgresDB.getCurrentRunners
+            runners.each do |key, runner|
+              # If the runner hasnt played that game, forget about it
+              if !runner.historic_runs.key?(gameID)
+                next
+              end
+              # Else we have to get the stats so we can correct those as well
+              game = runner.historic_runs[gameID]
+              runner.num_submitted_wrs -= game.num_previous_wrs
+              runner.num_submitted_runs -= game.num_submitted_runs
+              runner.total_time_overall -= game.total_time_overall
+              # Now delete the game
+              runner.historic_runs.delete(gameID)
+            end
+            # Update their fields
+            PostgresDB.updateCurrentRunners(runners)
+
+            # Delete the game and category aliases
+            conn.exec("DELETE from public.aliases where alias LIKE '#{_gameAlias}%'")
+
+            # Delete the game's resources
+            conn.exec("DELETE from public.resources where game_alias = '#{_gameAlias}'")
+          end # end of transaction
+
+        rescue Exception => e
+          _event << "Error while deleteing the game #{e.backtrace} #{e.message}"
+          next
+        end # end of begin
+
+        _event << "Game removed successfully"
+
       end # end of command body
 
-      def self.removeGameById(event, name)
-        begin
-          PostgresDB::Conn.prepare('statement1', 'select "game_id" from public."tracked_games" where "game_alias" = $1')
-          id = PostgresDB::Conn.exec_prepared('statement1', [name])
-
-          if id.empty? # If no ID's were found with entered ID.
-            event << 'No games found with this ID.'
-          else # Else remove game. ID is PK so it is guaranteed to be unique.
-            PostgresDB::Conn.prepare('statement2', 'delete * from public."tracked_games" where "game_id" = $1')
-            PostgresDB::Conn.exec_prepared('statement2', [id])
-          end
-        rescue Exception => e
-           RTBot.send_message(DevChannelID, e.backtrace.inspect + e.message)
-        end
-      end
     end
   end
 end
