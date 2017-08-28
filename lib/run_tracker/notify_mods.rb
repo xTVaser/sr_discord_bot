8caec6bce6224354c11ead701857c1c8d4913712
Tyler Wilding <xtvaser@gmail.com>
2017-08-27 22:08:03 -0400

    Should only notify current moderators

--- lib/run_tracker/notify_mods.rb
+++ lib/run_tracker/notify_mods.rb
@@ -34,7 +34,7 @@ module RunTracker
         actuallyNotified = false
         if message.length > 1
           trackedGame.moderators.each do |key, moderator|
-            if moderator.should_notify == true && moderator.discord_id != 0
+            if moderator.should_notify == true && moderator.discord_id != 0 && moderator.past_moderator == false
               actuallyNotified = true
               message.push("If you want to stop receiving these notifications reply with !optout #{moderator.src_name}")
               RTBot.user(moderator.discord_id).pm(Util.arrayToMessage(message))
